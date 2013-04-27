aedd84dc6d59e81f594aac77ea4502de1a4e4729
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2013-04-27 16:20:48 +0200

    add F001, L001 and L002 tests

---
+++ tests/F001.cpp
@@ -0,0 +1,7 @@
+// this file uses CRLF
+int i = 0;
+// a CR char is inserted in this line
+int j =1;
+//and the next line is ended by a LF alone
+int k = 0;
+
---
+++ tests/L002.cpp
@@ -0,0 +1,6 @@
+// one tab at the beginning of the next line 
+	int i = 1;
+// two tabs and a tab in the middle
+		int j =	0;
+// a tab in the middle of the line
+int k	= 1;
