bc7d11c04c94089106f9fda9291edbd15774e7d6
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2015-01-02 08:23:42 +0100

    exclude external projects from coverage

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -2,6 +2,9 @@ set(CTEST_CUSTOM_COVERAGE_EXCLUDE
   ${CTEST_CUSTOM_COVERAGE_EXCLUDE}
   # exclude cpptcl - this is an external lib used by vera++
   "/cpptcl"
+  "/boost-prefix"
+  "/lua-prefix"
+  "/luabind-prefix"
 )
 
 SET(CTEST_CUSTOM_WARNING_EXCEPTION
