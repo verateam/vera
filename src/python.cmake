29196d1f5d781f07b0d1f1d99b33d5ae111c9f83
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2015-01-14 11:24:59 +0100

    don't build python statically

--- src/python.cmake
+++ src/python.cmake
@@ -16,86 +16,86 @@ else()
   # a cmakeified version of python
   # prepare options for the extra modules
   set(opts)
-  foreach(m
-    ARRAY
-    AUDIOOP
-    BISECT
-    BZ2
-    CMATH
-    CODECS_CN
-    CODECS_HK
-    CODECS_ISO2022
-    CODECS_JP
-    CODECS_KR
-    CODECS_TW
-    COLLECTIONS
-    CPICKLE
-    CRYPT
-    CSTRINGIO
-    CSV
-    CTYPES_TEST
-    DATETIME
-    FCNTL
-    FUNCTOOLS
-    FUTURE_BUILTINS
-    GRP
-    HEAPQ
-    HOTSHOT
-    IO
-    ITERTOOLS
-    JSON
-    LINUXAUDIODEV
-    LOCALE
-    LSPROF
-    MATH
-    MMAP
-    MULTIBYTECODEC
-    MULTIPROCESSING
-    NIS
-    OPERATOR
-    OSSAUDIODEV
-    PARSER
-    RANDOM
-    RESOURCE
-    SELECT
-    SOCKET
-    SPWD
-    STROP
-    STRUCT
-    SYSLOG
-    TERMIOS
-    TIME
-    UNICODEDATA)
-    list(APPEND opts -DENABLE_${m}:BOOL=ON)
-    list(APPEND opts -DBUILTIN_${m}:BOOL=ON)
-  endforeach()
+  # foreach(m
+  #   ARRAY
+  #   AUDIOOP
+  #   BISECT
+  #   BZ2
+  #   CMATH
+  #   CODECS_CN
+  #   CODECS_HK
+  #   CODECS_ISO2022
+  #   CODECS_JP
+  #   CODECS_KR
+  #   CODECS_TW
+  #   COLLECTIONS
+  #   CPICKLE
+  #   CRYPT
+  #   CSTRINGIO
+  #   CSV
+  #   CTYPES_TEST
+  #   DATETIME
+  #   FCNTL
+  #   FUNCTOOLS
+  #   FUTURE_BUILTINS
+  #   GRP
+  #   HEAPQ
+  #   HOTSHOT
+  #   IO
+  #   ITERTOOLS
+  #   JSON
+  #   LINUXAUDIODEV
+  #   LOCALE
+  #   LSPROF
+  #   MATH
+  #   MMAP
+  #   MULTIBYTECODEC
+  #   MULTIPROCESSING
+  #   NIS
+  #   OPERATOR
+  #   OSSAUDIODEV
+  #   PARSER
+  #   RANDOM
+  #   RESOURCE
+  #   SELECT
+  #   SOCKET
+  #   SPWD
+  #   STROP
+  #   STRUCT
+  #   SYSLOG
+  #   TERMIOS
+  #   TIME
+  #   UNICODEDATA)
+  #   list(APPEND opts -DENABLE_${m}:BOOL=ON)
+  #   list(APPEND opts -DBUILTIN_${m}:BOOL=ON)
+  # endforeach()
   # disable modules that depends on extra libs
-  foreach(m
-    CURSES
-    CURSES_PANEL
-    READLINE
-    ELEMENTTREE
-    PYEXPAT
-    HASHLIB
-    SSL
-    MD5
-    SHA
-    SHA
-    SHA256
-    SHA512
-    SQLITE3
-    TKINTER
-    BINASCII
-    ZLIB
-    BSDDB
-    DBM
-    GDBM
-    # these two ones fail because of undefined symbols
-    SCPROXY
-    TESTCAPI)
-    list(APPEND opts -DENABLE_${m}:BOOL=OFF)
-    list(APPEND opts -DBUILTIN_${m}:BOOL=OFF)
-  endforeach()
+  # foreach(m
+  #   CURSES
+  #   CURSES_PANEL
+  #   READLINE
+  #   ELEMENTTREE
+  #   PYEXPAT
+  #   HASHLIB
+  #   SSL
+  #   MD5
+  #   SHA
+  #   SHA
+  #   SHA256
+  #   SHA512
+  #   SQLITE3
+  #   TKINTER
+  #   BINASCII
+  #   ZLIB
+  #   BSDDB
+  #   DBM
+  #   GDBM
+  #   # these two ones fail because of undefined symbols
+  #   SCPROXY
+  #   TESTCAPI)
+  #   list(APPEND opts -DENABLE_${m}:BOOL=OFF)
+  #   list(APPEND opts -DBUILTIN_${m}:BOOL=OFF)
+  # endforeach()
   ExternalProject_Add(python
     URL https://github.com/glehmann/python-cmake-buildsystem/archive/official-python-src.zip
     URL_MD5 b3e045924c2506287ba568aed28ca719
