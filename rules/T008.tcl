c0924d1f99ebd78da8c84a85f44672fedb2e6ed8
Loki Astari <Loki.Astari@gmail.com>
2016-04-05 00:14:27 -0700

    Add pp_error
        Keywords can appear in pp_error (as well as pp_pragma).
        We treat this two pre-processor tokens in the same way.

--- rules/T008.tcl
+++ rules/T008.tcl
@@ -2,15 +2,15 @@
 # Keywords catch, for, if and while should be followed by a single space
 
 foreach f [getSourceFileNames] {
-    set pp_pragma_line -1
-    foreach t [getTokens $f 1 0 -1 -1 {catch for if switch while pp_pragma}] {
+    set pp_line -1
+    foreach t [getTokens $f 1 0 -1 -1 {catch for if switch while pp_pragma pp_error}] {
         set keyword [lindex $t 0]
         set line [lindex $t 1]
         set column [lindex $t 2]
         set type [lindex $t 3]
-        if {$type == "pp_pragma"} {
-          set pp_pragma_line $line
-        } elseif {$pp_pragma_line != $line} {
+        if {($type == "pp_pragma") || ($type == "pp_error")} {
+          set pp_line $line
+        } elseif {$pp_line != $line} {
             set followingTokens [getTokens $f $line [expr $column + [string length $keyword]] [expr $line + 1] -1 {}]
             if {[llength $followingTokens] < 2} {
                 report $f $line "keyword '${keyword}' not followed by a single space"
