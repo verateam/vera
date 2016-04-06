1fd9ccd5791fe717d9bdccff0ad476324e013bcc
Loki Astari <Loki.Astari@gmail.com>
2016-04-06 01:15:17 -0700

    Rules for namespace in source file
        Gernally the statement:
    
            using namespace XXX;
    
        Is considered bad practice.
            See: http://stackoverflow.com/q/1452721/14065
                 "Why is “using namespace std” in C++ considered bad practice?"
    
        The exception is for X.cpp where X.h defines the namespace XXX
    
            X.h
            namespace XXX
            {
            }
    
            X.ccp
            using namespace XXX;       // OK
            using namespace YYY;       // Bad.

---
+++ rules/M003.tcl
@@ -0,0 +1,81 @@
+#!/usr/bin/tclsh
+# using namespace XXX is not allowed
+#   Unless you are in the source file X.cpp and the header file X.h contains the namespace XXX.
+
+proc getHeaderFile {fileName} {
+    return [join [list [file rootname $fileName] ".h"] ""]
+}
+proc getDefinedNamespaces {fileName} {
+    set headerFile [getHeaderFile $fileName]
+    set  namespaceList {}
+    set  currentNamespace {}
+    set  depth 0
+    set  depthHistory {}
+    set  state "start"
+    foreach token [getTokens $headerFile 1 0 -1 -1 {namespace identifier leftbrace rightbrace}] {
+        set tokenName [lindex $token 3]
+        set tokenValue [lindex $token 0]
+        if {$state == "start" && $tokenName == "namespace"} {
+            set state "namespace"
+        } elseif {$state == "namespace" && $tokenName == "identifier"} {
+            set state "namespace-identifier"
+            lappend currentNamespace $tokenValue
+            lappend depthHistory $depth
+            set $depth 0
+            lappend namespaceList $currentNamespace
+        } elseif {$state == "namespace-identifier" && $tokenName == "leftbrace"} {
+            set state "start"
+        } elseif {$state == "start" && $tokenName == "leftbrace"} {
+            set depth [expr $depth + 1]
+        } elseif {$state == "start" && $tokenName == "rightbrace"} {
+            if {$depth == 0} {
+                set depth [lindex $depthHistory end]
+                set depthHistory [lreplace $depthHistory end end]
+                set currentNamespace [lreplace $currentNamespace end end]
+            } else {
+                set depth [expr $depth - 1]
+            }
+        }
+    }
+    return $namespaceList;
+}
+
+proc validateUsing {file mark namespace definedNamespaces} {
+    set XX [lsearch $definedNamespaces $namespace]
+    if {[lsearch $definedNamespaces $namespace] == -1} {
+        set lineNumber [lindex $mark 1]
+        report $file $lineNumber "Namespace >$namespace< is not valid for using clause"
+    }
+}
+
+foreach fileName [getSourceFileNames] {
+    set extension [file extension $fileName]
+    if {[lsearch {.cpp .cc} $extension] != -1} {
+        set definedNamespaces [getDefinedNamespaces $fileName]
+        set state "start"
+        foreach token [getTokens $fileName 1 0 -1 -1 {using namespace identifier colon_colon semicolon}] {
+            set type [lindex $token 3]
+            set value [lindex $token 0]
+
+            if {$state == "start" && $type == "using"} {
+                set mark $token
+                set namespace {}
+                set state "using"
+            } elseif {$state == "using" && $type == "namespace"} {
+                set state "namespace"
+            } elseif {$state == "namespace" && $type == "identifier"} {
+                set state "identifier"
+                lappend namespace $value
+            } elseif {$state == "identifier" && $type == "colon_colon"} {
+                set state "colon_colon"
+            } elseif {$state == "colon_colon" && $type == "identifier"} {
+                set state "identifier"
+                lappend namespace $value
+            } elseif {$state == "identifier" && $type == "semicolon"} {
+                validateUsing $fileName $token $namespace $definedNamespaces
+                set state "start"
+            } else {
+            }
+        }
+    }
+}
